575aafc2fe6929e58ca1b7e773e8bcaecd9434e7
elleremo <elleremo@yandex.ru>
2020-03-13 17:26:25 +0300

    video

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+const log = console.log
+
+let videos = {
+  music: [
+    'https://www.youtube.com/watch?v=J3r-ct0haZg',
+    'https://www.youtube.com/watch?v=JnxhsWH1ir8',
+    'https://www.youtube.com/watch?v=_HPis2DUQgM'
+
+  ],
+  speach: [
+    'https://www.youtube.com/watch?v=EKjl3YL1eFg',
+    'https://www.youtube.com/watch?v=8OsGLTGPjBU',
+    'https://www.youtube.com/watch?v=cB4JPCFbBU8'
+  ],
+}
+
+function getVideoId (link){
+
+}
